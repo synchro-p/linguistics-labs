d3fb2ab87f8694f32956452cc778b183474c1ce5
Petr <p.samoseiko@g.nsu.ru>
2023-03-03 19:04:39 +0700

    working with filewise corpora v1

--- labs/lab1/src/main/java/Main.java
+++ labs/lab1/src/main/java/Main.java
@@ -6,5 +6,16 @@ public class Main {
         ArrayList<String> words = WordParser.parseRussianWordsFromCorpora(
                 Objects.requireNonNull(Main.class.getClassLoader().getResourceAsStream("corpora/samoseyko/output.txt")));
         System.out.println(words.size());
+        startup(400, "corpora/samoseyko/", ".txt");
+    }
+
+    private static void startup(int textsInCorpora, String prefix, String postfix) {
+        int sum = 0;
+        for (int i = 0; i < textsInCorpora; i++) {
+            String filenameBuilder = prefix + i + postfix;
+            ArrayList<String> words = WordParser.parseRussianWordsFromCorpora(
+                Objects.requireNonNull(Main.class.getClassLoader().getResourceAsStream(filenameBuilder)));
+
+        }
     }
 }
