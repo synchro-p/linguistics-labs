e0cb350adb08909fa3cdc0a6cdba8f96ad1eb0fb
Petr <p.samoseiko@g.nsu.ru>
2023-03-03 18:49:37 +0700

    added file-wise corpora representation

--- labs/lab1/src/main/java/Main.java
+++ labs/lab1/src/main/java/Main.java
@@ -4,7 +4,7 @@ import java.util.Objects;
 public class Main {
     public static void main(String[] args) {
         ArrayList<String> words = WordParser.parseRussianWordsFromCorpora(
-                Objects.requireNonNull(Main.class.getClassLoader().getResourceAsStream("output.txt")));
+                Objects.requireNonNull(Main.class.getClassLoader().getResourceAsStream("corpora/samoseyko/output.txt")));
         System.out.println(words.size());
     }
 }
