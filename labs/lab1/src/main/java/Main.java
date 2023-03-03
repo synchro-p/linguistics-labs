380f2293465b50e4f8d24ffe231c27a98c9ddfe2
Bragunetzki <dragonbolty@gmail.com>
2023-03-03 13:20:47 +0700

    Added Lemmatizer

--- labs/lab1/src/main/java/Main.java
+++ labs/lab1/src/main/java/Main.java
@@ -1,10 +1,24 @@
-import java.util.ArrayList;
-import java.util.Objects;
+package main.java;
+
+import javax.xml.stream.XMLStreamException;
+import java.io.FileNotFoundException;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<String> words = WordParser.parseRussianWordsFromCorpora(
+        /*ArrayList<String> words = WordParser.parseRussianWordsFromCorpora(
                 Objects.requireNonNull(Main.class.getClassLoader().getResourceAsStream("output.txt")));
-        System.out.println(words.size());
+        System.out.println(words.size());*/
+
+        String[] strings = new String[]{"пельменя", "пельменю", "картошкой", "кушала", "бумажные"};
+
+        Lemmatizer lemmatizer;
+        try {
+            lemmatizer = new Lemmatizer("dict.opcorpora");
+            for (String s : strings) {
+                System.out.println("Original: " + s + " --> Lemma: " + lemmatizer.findLemmaForm(s));
+            }
+        } catch (FileNotFoundException | XMLStreamException e) {
+            e.printStackTrace();
+        }
     }
 }
