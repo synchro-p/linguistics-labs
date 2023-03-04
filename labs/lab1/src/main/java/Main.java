e129ef91118337964bac3e1267f900cca1df3bae
Petr <p.samoseiko@g.nsu.ru>
2023-03-04 19:08:24 +0700

    working with filewise corpora v2

--- labs/lab1/src/main/java/Main.java
+++ labs/lab1/src/main/java/Main.java
@@ -12,12 +12,20 @@ public class Main {
     }
 
     private static void startup(int textsInCorpora, String prefix, String postfix) {
-        int sum = 0;
+        Lemmatizer lemmatizer;
+        try {
+            lemmatizer = new Lemmatizer("dict.opcorpora.xml");
+        } catch (FileNotFoundException | XMLStreamException e) {
+            throw new RuntimeException("could not access specified dictionary");
+        }
         for (int i = 0; i < textsInCorpora; i++) {
             String filenameBuilder = prefix + i + postfix;
             ArrayList<String> words = WordParser.parseRussianWordsFromCorpora(
                 Objects.requireNonNull(Main.class.getClassLoader().getResourceAsStream(filenameBuilder)));
-
+            for (String s : words) {
+                System.out.println("Original: " + s + " --> Lemma: " + lemmatizer.findLemmaForm(s));
+            }
+            System.out.println("*********");
         }
     }
 }
