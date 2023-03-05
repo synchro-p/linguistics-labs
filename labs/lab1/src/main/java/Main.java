b2e52a40459987b53bdac26f12dff8401a35ddc4
Petr <p.samoseiko@g.nsu.ru>
2023-03-05 14:55:06 +0700

    implemented statistics counters

--- labs/lab1/src/main/java/Main.java
+++ labs/lab1/src/main/java/Main.java
@@ -1,39 +1,49 @@
 import javax.xml.stream.XMLStreamException;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) {
-        String s = "какой-то";
-        try {
-            Lemmatizer lemmatizer = new Lemmatizer("dict.opcorpora.xml");
-            System.out.println(lemmatizer.findLemmaForm(s));
-        } catch (XMLStreamException | FileNotFoundException e) {
-            e.printStackTrace();
-        }
-
-/*        ArrayList<String> words = WordParser.parseRussianWordsFromCorpora(
-                Objects.requireNonNull(Main.class.getClassLoader().getResourceAsStream("corpora/samoseyko/output.txt")));
-        System.out.println(words.size());
-        startup(400, "corpora/samoseyko/", ".txt");*/
+//        String s = "какой-то";
+//        try {
+//            Lemmatizer lemmatizer = new Lemmatizer("dict.opcorpora.xml");
+//            System.out.println(lemmatizer.findLemmaForm(s));
+//        } catch (XMLStreamException | FileNotFoundException e) {
+//            e.printStackTrace();
+//        }
+        startup(2, "corpora/samoseyko/", ".txt");
     }
 
     private static void startup(int textsInCorpora, String prefix, String postfix) {
+        int sum = 0;
         Lemmatizer lemmatizer;
         try {
             lemmatizer = new Lemmatizer("dict.opcorpora.xml");
         } catch (FileNotFoundException | XMLStreamException e) {
             throw new RuntimeException("could not access specified dictionary");
         }
+        HashMap<Lemma, StatisticContainer> freqDictionary = new HashMap<>();
         for (int i = 0; i < textsInCorpora; i++) {
             String filenameBuilder = prefix + i + postfix;
             ArrayList<String> words = WordParser.parseRussianWordsFromCorpora(
                 Objects.requireNonNull(Main.class.getClassLoader().getResourceAsStream(filenameBuilder)));
             for (String s : words) {
-                System.out.println("Original: " + s + " --> Lemma: " + lemmatizer.findLemmaForm(s));
+                Lemma lemma = lemmatizer.findLemma(s);
+                StatisticContainer stats = freqDictionary.getOrDefault(lemma, new StatisticContainer(textsInCorpora));
+                stats.incrementOccurrencesInText(i);
+                freqDictionary.put(lemma, stats);
+                sum++;
             }
-            System.out.println("*********");
+        }
+
+        for (StatisticContainer stat : freqDictionary.values()) {
+            stat.calculateTotalStats(sum);
+        }
+
+        for (Lemma s : freqDictionary.keySet()) {
+            System.out.println(s.getLemmaForm() + ": " + freqDictionary.get(s).toString());
         }
     }
 }
