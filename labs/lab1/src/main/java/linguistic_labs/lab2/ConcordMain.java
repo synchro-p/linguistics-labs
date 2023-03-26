3aa2c4997eb5452f1608a13765b6083b5a1ebf28
Petr <p.samoseiko@g.nsu.ru>
2023-03-26 15:57:05 +0700

    implemented tokenization and normalization for concordance search

---
+++ labs/lab1/src/main/java/linguistic_labs/lab2/ConcordMain.java
@@ -0,0 +1,35 @@
+package linguistic_labs.lab2;
+
+import linguistic_labs.commons.freq.CsvFrequencyDictionaryReader;
+import linguistic_labs.commons.freq.Disambiguator;
+import linguistic_labs.commons.freq.FrequencyDictionary;
+import linguistic_labs.commons.lemma.Lemmatizer;
+
+import javax.xml.stream.XMLStreamException;
+import java.io.ByteArrayInputStream;
+import java.io.FileNotFoundException;
+import java.nio.charset.StandardCharsets;
+
+public class ConcordMain {
+    public static void main(String[] args) {
+        Lemmatizer lemmatizer;
+        try {
+            lemmatizer = new Lemmatizer("dict.opcorpora.xml");
+        } catch (FileNotFoundException | XMLStreamException e) {
+            throw new RuntimeException("could not access specified dictionary");
+        }
+
+        FrequencyDictionary frequencyDictionary =
+                new CsvFrequencyDictionaryReader(
+                        ConcordMain.class.getClassLoader().getResourceAsStream("freqrnc2011.csv")).parseFrequencies();
+        Disambiguator disambiguator = new Disambiguator(frequencyDictionary);
+
+        ConcordTokenizer tokenizer = new ConcordTokenizer();
+        ConcordNormalizer normalizer = new ConcordNormalizer(lemmatizer, disambiguator);
+
+        String a = "мама мыла раму собарнаяавы";
+
+        tokenizer.tokenizeText(new ByteArrayInputStream(a.getBytes(StandardCharsets.UTF_8)));
+        System.out.println(normalizer.normalizeWords(tokenizer.getWords()));
+    }
+}
